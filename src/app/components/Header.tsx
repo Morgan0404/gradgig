873354749dc37b4458c2ab9a893acefceebc89fc
Morgan James <mj522@exeter.ac.uk>
2024-08-21 17:13:07 +0100

    deleted duplicate .env

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -7,7 +7,7 @@ export default async function Header() {
     return (
         <header>
             <div className="container flex items-center justify-between mx-auto my-4">
-                <Link href={'/'} className="font-bold text-xl">Flexi-Work</Link>
+                <Link href={'/'} className="font-bold text-xl">Grad Gig</Link>
                 <nav className="flex gap-2">
                     {!user && (
                         <Link className="rounded-md bg-gray-200 py-2 px-4 rounded-md text-center" href={signInUrl}>

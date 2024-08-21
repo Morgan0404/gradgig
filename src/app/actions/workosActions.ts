33aff3130b671e1c184ad83dee761b68bff98c95
Morgan James <mj522@exeter.ac.uk>
2024-08-21 18:30:02 +0100

    adding roleSlug

--- src/app/actions/workosActions.ts
+++ src/app/actions/workosActions.ts
@@ -1,17 +1,18 @@
 'use server';
-import { WorkOS } from "@workos-inc/node";
-import {redirect} from "next/navigation";
+
+import {WorkOS} from "@workos-inc/node";
 import {revalidatePath} from "next/cache";
+import {redirect} from "next/navigation";
 
 const workos = new WorkOS(process.env.WORKOS_API_KEY);
 
-export async function createCompany(companyName: string, userId: string) {
-    const org = await workos.organizations.createOrganization({ name: companyName });
+export async function createCompany(companyName: string, userId:string) {
+    const org = await workos.organizations.createOrganization({name: companyName});
     await workos.userManagement.createOrganizationMembership({
         userId,
-        organizationId: org.id, // Corrected parameter name
-        role: 'admin'
+        organizationId: org.id,
+        roleSlug: 'admin',
     });
-    revalidatePath('/new-listing')
+    revalidatePath('/new-listing');
     redirect('/new-listing');
 }

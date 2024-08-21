447d20ae286101f5e142f54f5fc1b8e9fcea60ee
Morgan James <mj522@exeter.ac.uk>
2024-08-21 18:39:32 +0100

    g

--- src/app/components/ImageUpload.tsx
+++ src/app/components/ImageUpload.tsx
@@ -1,75 +1,68 @@
 'use client';
-
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faSpinner,faUser, IconDefinition } from "@fortawesome/free-solid-svg-icons";
-import { Button } from "@radix-ui/themes";
-import {useRef, useState} from "react";
-import Image from "next/image";
+import {faSpinner, faUser, IconDefinition} from "@fortawesome/free-solid-svg-icons";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {Button} from "@radix-ui/themes";
 import axios from "axios";
+import Image from "next/image";
+import {ChangeEvent, useRef, useState} from "react";
 
 export default function ImageUpload({
                                         name,icon,defaultValue='',
-
-    }: {
+                                    }:{
     name:string;
-    icon: IconDefinition;
+    icon:IconDefinition;
     defaultValue:string;
-
-
-    }) {
+}) {
     const fileInRef = useRef<HTMLInputElement>(null);
-    const [isUploading,setIsUploading] = useState(false);
-    const [isImageLoading, setIsImageLoading] = useState(false)
+    const [isUploading, setIsUploading] = useState(false);
+    const [isImageLoading, setIsImageLoading] = useState(false);
     const [url, setUrl] = useState(defaultValue);
 
     async function upload(ev:ChangeEvent<HTMLInputElement>) {
         const input = ev.target as HTMLInputElement;
         if (input && input.files?.length && input.files.length > 0) {
-
+            setIsUploading(true);
             const file = input.files[0];
-            const data = new FormData();
+            const data = new FormData;
             data.set('file', file);
             const response = await axios.post('/api/upload', data);
-
             if (response.data.url) {
                 setUrl(response.data.url);
                 setIsUploading(false);
                 setIsImageLoading(true);
-
-
             }
         }
     }
+
     const imgLoading = (isUploading || isImageLoading);
+
     return (
         <>
-            <div className="bg-gray-100 rounded-md size-24 inline-flex items-center content-center justify-center" >
+            <div className="bg-gray-100 rounded-md size-24 inline-flex items-center content-center justify-center">
                 {imgLoading && (
-                    <FontAwesomeIcon icon={faSpinner} className="text-gray-400 animate-spin" />
+                    <FontAwesomeIcon icon={faSpinner} className="text-gray-400 animate-spin"/>
                 )}
                 {(!isUploading) && url && (
                     <Image src={url} alt={'uploaded image'} width={1024} height={1024}
-                           onLoad={() => setIsImageLoading(false)}
-                           className="w-auto h-auto max-w-24 max-h-24"/>
+                           onLoadingComplete={() => setIsImageLoading(false)}
+                           className="w-auto h-auto max-w-24 max-h-24" />
                 )}
                 {!imgLoading && !url && (
-                    <FontAwesomeIcon icon={icon} className="text-gray-400" />
-
+                    <FontAwesomeIcon icon={icon} className="text-gray-400"/>
                 )}
-
             </div>
-            <input type="hidden" value={url} name = {name}/>
+            <input type="hidden" value={url} name={name} />
             <div className="mt-2">
                 <input
-                    onChange= {ev =>upload(ev)}
+                    onChange={ev => upload(ev)}
                     ref={fileInRef}
                     type="file"
-                    className="hidden" />
+                    className="hidden"/>
                 <Button
                     type="button"
                     onClick={() => fileInRef.current?.click()}
                     variant="soft">
-                    Select file
+                    select file
                 </Button>
             </div>
         </>

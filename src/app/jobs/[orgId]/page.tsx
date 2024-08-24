f69abbb148da103541837efec03783dbe7512d19
Morgan James <mj522@exeter.ac.uk>
2024-08-24 21:55:53 +0100

    debugging serverside error

--- src/app/jobs/[orgId]/page.tsx
+++ src/app/jobs/[orgId]/page.tsx
@@ -1,10 +1,8 @@
 import Jobs from "@/app/components/Jobs";
-import {getUser} from "@workos-inc/authkit-nextjs";
+import { getUser } from "@workos-inc/authkit-nextjs";
 import mongoose from "mongoose";
 import { JobModel, addOrgAndUserData } from "@/models/Job";
-import {AutoPaginatable, OrganizationMembership, WorkOS} from "@workos-inc/node";
-
-
+import { WorkOS } from "@workos-inc/node";
 
 type PageProps = {
     params: {
@@ -12,21 +10,33 @@ type PageProps = {
     }
 };
 
+export default async function CompanyJobsPage(props: PageProps) {
+    console.log("Received orgId:", props.params.orgId);
 
-export default async function CompanyJobsPage(props:PageProps){
+    if (!props.params.orgId) {
+        throw new Error("Organization ID is missing or undefined");
+    }
+
     const workos = new WorkOS(process.env.WORKOS_API_KEY);
-    const org = await workos.organizations.getOrganization(props.params.orgId);
-    const {user} = await getUser();
-    let jobsDocs = JSON.parse(JSON.stringify(await JobModel.find({orgId: org.id})));
+
+    let org;
+    try {
+        org = await workos.organizations.getOrganization(props.params.orgId);
+    } catch (error) {
+        console.error("Failed to retrieve organization:", error);
+        throw new Error("Failed to retrieve organization");
+    }
+
+    const { user } = await getUser();
+    let jobsDocs = JSON.parse(JSON.stringify(await JobModel.find({ orgId: org.id })));
     jobsDocs = await addOrgAndUserData(jobsDocs, user);
 
-    return(
+    return (
         <div>
             <div className="container">
                 <h1 className="text-xl my-6">{org.name} Jobs</h1>
             </div>
-            <Jobs jobs={jobsDocs} header={"Jobs posted by "+org.name} />
+            <Jobs jobs={jobsDocs} header={"Jobs posted by " + org.name} />
         </div>
     );
-
 }
